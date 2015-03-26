897e5a758bf9596d09d957b9bac85e9c91ceb1bb
Anne-Sophie Tranchet <astranchet@tea-ebook.com>
2015-03-26 10:32:02 +0100

    Add: tests on existing fixtures

--- test/test_im_onix.rb
+++ test/test_im_onix.rb
@@ -109,6 +109,18 @@ class TestImOnix < Minitest::Test
     end
   end
 
+  context 'master part of "Certaines n’avaient jamais vu la mer"' do
+    setup do
+      @message = ONIX::ONIXMessage.new
+      @message.parse("test/fixtures/9782752906700.xml")
+      @product=@message.products.first
+    end
+
+    should "be streaming" do
+      assert @product.streaming?
+    end
+  end
+
   context 'epub part of "Certaines n’avaient jamais vu la mer"' do
     setup do
       @message = ONIX::ONIXMessage.new
